c8770a23c9dbedc0f0233cf32bcad4f665798883
KarolisVenckus <karolis.venckus12@gmail.com>
2023-04-13 14:01:26 +0300

    day 3

---
+++ Quiz.py
@@ -0,0 +1,1 @@
+welcome = input("Welcome to the music quiz, u will be asked 5 questions, and u have to give an answer!")
--- karolis_uzduotis.py
+++ karolis_uzduotis.py
@@ -1,23 +1,27 @@
-Pinigu_Tipas = input("Pasirinkite kokia valiuta jus norite iskeisti (Svarams S, Eurams E)! ")
-Suma = input("Iveskite suma! ")
-if Pinigu_Tipas == "S":
-    Eurai = float(Suma) * 1.14
-    print(f"Iskeitus pinigus jum liko {Eurai:2f}! ")
-elif Pinigu_Tipas == "E":
-    Svarai = float(Suma) / 0.88
-    print(f"Iskeitus pinigus jum liko {Svarai:2f}!")
+pinigu_tipas = input("Pasirinkite kokia valiuta jus norite iskeisti (Svarams S, Eurams E)! ")
+suma = input("Iveskite suma! ")
+if pinigu_tipas == "S":
+    eurai = float(suma) * 1.14
+    print(f"Iskeitus pinigus jum liko {eurai:2f}! ")
+elif pinigu_tipas == "E":
+    svarai = float(suma) * 0.88
+    print(f"Iskeitus pinigus jum liko {svarai:2f}!")
 else:
     print(f"Jus ivedete netinkama valiuta. ")
 
 print(f"Aciu kad naudojote mano konverteri! \U0001F600")
 
-Ginklas = input("Jus uzpuole monstras! Pasirinkite ginkla! (Kumstis, Kardas arba Lankas): ")
-if Ginklas == "Kumstis":
+
+ginklas = input("Jus uzpuole monstras! Pasirinkite ginkla! (Kumstis, Kardas arba Lankas): ")
+if ginklas == "Kumstis":
     print(f"Jus trenkete monstrui, bet jis nieko nepajuto... \U0001F923")
-elif Ginklas == "Kardas":
+    print("-------------------Jus pralaimejote!------------------------")
+elif ginklas == "Kardas":
     print(f"Jus smogete monstrui kardu, ir jus ji iveikete! \U0001f600")
-elif Ginklas == "Lankas":
+    print("-------------------Jus laimejote!--------------")
+elif ginklas == "Lankas":
     print(f"Jus saunate i monstra su lanku, bet jis pagavo strele ir mete ja i jus... \U0001F606")
+    print("---------------------------Jus pralaimejote!----------------------------")
 else:
     print(f"Netinkamas ginklo paisirinkimas. ")
 
