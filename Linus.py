a5ac7d760cee01b511b0410963898595815baefa
KarolisVenckus <karolis.venckus12@gmail.com>
2023-04-17 10:46:56 +0300

    darbas

--- Linus.py
+++ Linus.py
@@ -1,11 +1,4 @@
-First_number = input("Type in a number: ")
-Second_number = input("Type in a second number: ")
-Third_number = input("Type in a third number: ")
-Biggest_number = 0
-if int(First_number) > int(Biggest_number):
-    Biggest_number = First_number
-if int(Second_number) > int(Biggest_number):
-    Biggest_number = Second_number
-if int(Third_number) > int(Biggest_number):
-    Biggest_number = Third_number
-print(f"The highest number is {Biggest_number}")
+for x in range(6):
+    for y in range(6):
+        print(x * y)
+
